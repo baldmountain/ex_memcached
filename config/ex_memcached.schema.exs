8fe2fd5eef21f16ee7be69979f960bec173eb547
Geoffrey Clements <gclements@kayak.com>
2014-07-07 16:33:22 -0400

    Some exrm files.

---
+++ config/ex_memcached.schema.exs
@@ -0,0 +1,28 @@
+[
+  mappings: [
+    "ex_memcached.max_data_size": [
+      doc: "Documentation for ex_memcached.max_data_size goes here.",
+      to: "ex_memcached.max_data_size",
+      datatype: :integer,
+      default: 1048576
+    ],
+    "ex_memcached.listen_port": [
+      doc: "Documentation for ex_memcached.listen_port goes here.",
+      to: "ex_memcached.listen_port",
+      datatype: :integer,
+      default: 11211
+    ],
+    "ex_memcached.max_connections": [
+      doc: "Documentation for ex_memcached.max_connections goes here.",
+      to: "ex_memcached.max_connections",
+      datatype: :integer,
+      default: 1024
+    ],
+    "ex_memcached.disable_flush_all": [
+      doc: "Documentation for ex_memcached.disable_flush_all goes here.",
+      to: "ex_memcached.disable_flush_all",
+      datatype: :atom,
+      default: false]
+    ],
+  translations: []
+]
